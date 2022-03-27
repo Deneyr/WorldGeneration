bb3b329509281f9d7bb88d04665feb2dcd48a90a
Deneyr <ihzazsh@gmail.com>
2022-03-27 17:56:18 +0200

    Create own cubic bezier filter

--- WorldGeneration/DataChunks/WeatherMonitoring/AltitudeBiomeMonitor.cs
+++ WorldGeneration/DataChunks/WeatherMonitoring/AltitudeBiomeMonitor.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿using SFML.System;
+using System;
 using System.Collections.Generic;
-using System.Geometry;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using WorldGeneration.Maths;
 
 namespace WorldGeneration.DataChunks.WeatherMonitoring
 {
     internal class AltitudeBiomeMonitor
     {
-        private Dictionary<BiomeType, Bezier> biomeTypeToBezierCurve;
+        private Dictionary<BiomeType, CubicBezierCurve> biomeTypeToBezierCurve;
 
         public AltitudeBiomeMonitor()
         {
-            this.biomeTypeToBezierCurve = new Dictionary<BiomeType, Bezier>();
+            this.biomeTypeToBezierCurve = new Dictionary<BiomeType, CubicBezierCurve>();
 
-            this.biomeTypeToBezierCurve.Add(BiomeType.DESERT, new Bezier(new Vector2(0, 0), new Vector2(0.294f, 0.097f), new Vector2(0.833f, 0.198f), new Vector2(1f, 0.2f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.SAVANNA, new Bezier(new Vector2(0, 0), new Vector2(0.34f, 0.09f), new Vector2(0.817f, 0.98f), new Vector2(1f, 0.1f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.TROPICAL_WOODLAND, new Bezier(new Vector2(0, 0), new Vector2(0.15f, 0.318f), new Vector2(0.726f, 0.2f), new Vector2(1f, 0.1f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.TUNDRA, new Bezier(new Vector2(0, 0), new Vector2(0.561f, 1f), new Vector2(0.35f, 1f), new Vector2(1f, 1f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.SEASONAL_FOREST, new Bezier(new Vector2(0, 0), new Vector2(0.736f, 0.532f), new Vector2(0.382f, 0.443f), new Vector2(1f, 0.32f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.RAINFOREST, new Bezier(new Vector2(0, 0), new Vector2(0.272f, 0.075f), new Vector2(0.65f, 1f), new Vector2(1f, 1f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.TEMPERATE_FOREST, new Bezier(new Vector2(0, 0), new Vector2(0.718f, 0.55f), new Vector2(0.401f, 0.43f), new Vector2(1f, 0.3f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.TEMPERATE_RAINFOREST, new Bezier(new Vector2(0, 0), new Vector2(0.677f, 0f), new Vector2(0f, 1f), new Vector2(1f, 1f)));
-            this.biomeTypeToBezierCurve.Add(BiomeType.BOREAL_FOREST, new Bezier(new Vector2(0, 0), new Vector2(0.406f, 0.04f), new Vector2(0.698f, 0.075f), new Vector2(1f, 0.03f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.DESERT, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.294f, 0.097f), new Vector2f(0.833f, 0.198f), new Vector2f(1f, 0.2f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.SAVANNA, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.34f, 0.09f), new Vector2f(0.817f, 0.98f), new Vector2f(1f, 0.1f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.TROPICAL_WOODLAND, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.15f, 0.318f), new Vector2f(0.726f, 0.2f), new Vector2f(1f, 0.1f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.TUNDRA, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.561f, 1f), new Vector2f(0.35f, 1f), new Vector2f(1f, 1f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.SEASONAL_FOREST, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.736f, 0.532f), new Vector2f(0.382f, 0.443f), new Vector2f(1f, 0.32f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.RAINFOREST, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.272f, 0.075f), new Vector2f(0.65f, 1f), new Vector2f(1f, 1f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.TEMPERATE_FOREST, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.718f, 0.55f), new Vector2f(0.401f, 0.43f), new Vector2f(1f, 0.3f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.TEMPERATE_RAINFOREST, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.677f, 0f), new Vector2f(0f, 1f), new Vector2f(1f, 1f)));
+            this.biomeTypeToBezierCurve.Add(BiomeType.BOREAL_FOREST, new CubicBezierCurve(new Vector2f(0, 0), new Vector2f(0.406f, 0.04f), new Vector2f(0.698f, 0.075f), new Vector2f(1f, 0.03f)));
         }
 
         public float FilterAltitudeFromBiome(BiomeType biomeType, float altitude)
@@ -55,30 +57,9 @@ namespace WorldGeneration.DataChunks.WeatherMonitoring
             //}
             //return altitude;
 
-            Bezier filterToApply = this.biomeTypeToBezierCurve[biomeType];
-            float test = 0;
-            if (altitude > 0.1)
-            {
-                test = -2;
-            }
+            CubicBezierCurve filterToApply = this.biomeTypeToBezierCurve[biomeType];
 
-            //if(altitude < 0.01)
-            //{
-            //    altitude = 0;
-            //}
-            //else if(altitude > 0.99)
-            //{
-            //    altitude = 1f;
-            //}
-
-            float[] intersections = filterToApply.Intersects(new Line(new Vector2(altitude, test), new Vector2(altitude, 2f)));
-
-            if (intersections.Length == 0)
-            {
-                return altitude;
-            }
-
-            return filterToApply.Position(filterToApply.Intersects(new Line(new Vector2(altitude, test), new Vector2(altitude, 2f)))[0]).Y;
+            return Math.Abs(filterToApply.ImageAt(altitude));
         }
     }
 }
---
+++ WorldGeneration/Maths/CubicBezierCurve.cs
@@ -0,0 +1,126 @@
+﻿using MathNet.Numerics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGeneration.Maths
+{
+    internal class CubicBezierCurve
+    {
+        public Vector2f Point0
+        {
+            get;
+            private set;
+        }
+
+        public Vector2f Point1
+        {
+            get;
+            private set;
+        }
+
+        public Vector2f Point2
+        {
+            get;
+            private set;
+        }
+
+        public Vector2f Point3
+        {
+            get;
+            private set;
+        }
+
+        public CubicBezierCurve(Vector2f point0, Vector2f point1, Vector2f point2, Vector2f point3)
+        {
+            this.Point0 = point0;
+            this.Point1 = point1;
+            this.Point2 = point2;
+            this.Point3 = point3;
+        }
+
+        public Vector2f PointAt(float t)
+        {
+            float a = 1 - t;
+            float a2 = a * a;
+            float a3 = a2 * a;
+
+            float t2 = t * t;
+            float t3 = t2 * t;
+
+            float x = a3 * this.Point0.X + 3 * a2 * t * this.Point1.X + 3 * a * t2 * this.Point2.X + t3 * this.Point3.X;
+            float y = a3 * this.Point0.Y + 3 * a2 * t * this.Point1.Y + 3 * a * t2 * this.Point2.Y + t3 * this.Point3.Y;
+
+            return new Vector2f(x, y);
+        }
+
+        public float ImageAt(float x)
+        {
+            // 3*p1 - p0 - 3*p2 + p3
+            float a = 3 * this.Point1.X - this.Point0.X - 3 * this.Point2.X + this.Point3.X;
+
+            // 3*p0 - 6*p1 + 3*p2
+            float b = 3 * this.Point0.X - 6 * this.Point1.X + 3 * this.Point2.X;
+
+            // 3*p1 - 3*p0
+            float c = 3 * this.Point1.X - 3 * this.Point0.X;
+
+            // p0 - x
+            float d = this.Point0.X - x;
+
+            var roots = FindRoots.Cubic(d, c, b, a);
+
+            float t = 0;
+            if (roots.Item1.Real >= 0 && roots.Item1.Real <= 1)
+            {
+                t = (float)roots.Item1.Real;
+            }
+            else if (roots.Item2.Real >= 0 && roots.Item2.Real <= 1)
+            {
+                t = (float)roots.Item2.Real;
+            }
+            else if (roots.Item3.Real >= 0 && roots.Item3.Real <= 1)
+            {
+                t = (float)roots.Item3.Real;
+            }
+            else
+            {
+                //if (roots.Item1.Imaginary == -roots.Item2.Imaginary)
+                //{
+                //    t = (float)roots.Item3.Real;
+                //}
+                //else if (roots.Item1.Imaginary == -roots.Item3.Imaginary)
+                //{
+                //    t = (float)roots.Item2.Real;
+                //}
+                //else
+                //{
+                //    if (roots.Item1.Imaginary == roots.Item2.Imaginary)
+                //    {
+                //        t = (float)roots.Item3.Real;
+                //    }
+                //    else if (roots.Item1.Imaginary == roots.Item3.Imaginary)
+                //    {
+                //        t = (float)roots.Item2.Real;
+                //    }
+                //    else
+                //    {
+                //        t = (float)roots.Item1.Real;
+                //    }
+                //}
+                throw new Exception("No real solution to bezier curve equation");
+            }
+
+            float a1 = 1 - t;
+            float a2 = a1 * a1;
+            float a3 = a2 * a1;
+
+            float t2 = t * t;
+            float t3 = t2 * t;
+            return a3 * this.Point0.Y + 3 * a2 * t * this.Point1.Y + 3 * a1 * t2 * this.Point2.Y + t3 * this.Point3.Y;
+        }
+    }
+}
